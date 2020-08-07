2226fd818faee68e9b271af9ebd1754bf096c785
Doug Schallmoser <dougschallmoser@mgail.com>
2020-08-06 22:06:38 -0700

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,7 +6,7 @@ class Show < ActiveRecord::Base
     end 
 
     def self.most_popular_show
-        self.where(rating: self.highest_rating).first
+        self.where("rating = ?", self.highest_rating).first
     end 
 
     def self.lowest_rating
@@ -14,7 +14,7 @@ class Show < ActiveRecord::Base
     end
 
     def self.least_popular_show
-        self.where(rating: self.lowest_rating).first
+        self.where("rating = ?", self.lowest_rating).first
     end
 
     def self.ratings_sum
